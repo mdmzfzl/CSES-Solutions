38db71c1d816b2a25a1a102fbb7f43e21170cb48
mdmzfzl <mmuzakkir244@gmail.com>
2023-11-18 10:02:07 +0300

    Added String Algorithms solutions

---
+++ 08_String_Algorithms/1753-string-matching.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+#define ll long long
+#define vi vector<int>
+#define vl vector<long long>
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define umap unordered_map
+#define uset unordered_set
+
+#define rall(x) (x).rbegin(), (x).rend()
+#define all(x) (x).begin(), (x).end()
+#define pb push_back
+#define mp make_pair
+
+const long long LLINF = LLONG_MAX;
+const int INF = INT_MAX;
+const int MOD = 1e9 + 7;
+
+vi zFunction(const string& s) {
+    int n = s.length();
+    vi z(n, 0);
+
+    int l = 0, r = 0;
+    for (int i = 1; i < n; i++) {
+        if (i <= r) {
+            z[i] = min(r - i + 1, z[i - l]);
+        }
+        while (i + z[i] < n && s[z[i]] == s[i + z[i]]) {
+            z[i]++;
+        }
+        if (i + z[i] - 1 > r) {
+            l = i;
+            r = i + z[i] - 1;
+        }
+    }
+
+    return z;
+}
+
+void solve() {
+    string T, P;
+    cin >> T >> P;
+
+    string concat = P + "$" + T;
+    vi z = zFunction(concat);
+
+    int len = P.length();
+    int result = 0;
+    for (size_t i = len + 1; i < z.size(); i++) {
+        if (z[i] == len) {
+            result++;
+        }
+    }
+
+    cout << result << '\n';
+}
+
+int main() {
+    fastio
+
+    int tc = 1; // Number of test cases
+    // cin >> tc;
+
+    while (tc--) {
+        solve();
+    }
+
+    return 0;
+}
