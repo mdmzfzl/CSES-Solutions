6c07b23c86a0cce283e6bb557801b963e16db187
mdmzfzl <mmuzakkir244@gmail.com>
2023-11-24 13:36:27 +0300

    Added String Algorithms solutions

---
+++ 08_String_Algorithms/1733-finding-periods.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+#define ll long long
+#define vi vector<int>
+#define vl vector<long long>
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define umap unordered_map
+#define uset unordered_set
+
+#define rall(x) (x).rbegin(), (x).rend()
+#define all(x) (x).begin(), (x).end()
+#define pb push_back
+#define mp make_pair
+
+const long long LLINF = LLONG_MAX;
+const int INF = INT_MAX;
+const int MOD = 1e9 + 7;
+
+vi zFunction(const string& s) {
+    int n = s.size();
+    vi z(n, 0);
+
+    for (int i = 1, l = 0, r = 0; i < n; i++) {
+        if (i <= r)
+            z[i] = min(z[i - l], r - i + 1);
+
+        while (i + z[i] < n && s[z[i]] == s[i + z[i]])
+            z[i]++;
+
+        if (i + z[i] - 1 > r)
+            l = i, r = i + z[i] - 1;
+    }
+
+    return z;
+}
+
+void solve() {
+    string s;
+    cin >> s;
+
+    vi z = zFunction(s);
+
+    for (size_t i = 0; i < s.size(); i++)
+        if (i + z[i] == s.size())
+            cout << i << ' ';
+    cout << s.size() << '\n';
+}
+
+int main() {
+    fastio
+
+    int tc = 1; // Number of test cases
+    // cin >> tc;
+
+    while (tc--) {
+        solve();
+    }
+
+    return 0;
+}
